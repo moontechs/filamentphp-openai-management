a45318a0ed1be8672d840072d5cce54f8c2ecee9
mkoziy <mkoziy@users.noreply.github.com>
2024-09-08 16:47:07 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,9 +14,9 @@ use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
+use Moontechs\OpenaiBatchesManagement\OpenaiBatchesManagementServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Moontechs\OpenaiBatchesManagement\OpenaiBatchesManagementServiceProvider;
 
 class TestCase extends Orchestra
 {
